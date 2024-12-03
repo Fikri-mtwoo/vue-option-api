68303d78df89b5a2941c411d84e4879b55cac43b
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-03 15:03:36 +0700

    methods

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,23 @@
   <!-- deklarative rendering -->
    <div>{{ message }}</div>
    <span :title="message">saya sedang belajar vue 3</span>
+
+   <!-- methods -->
+    <div>{{ count }}</div>
+    <button @click="count++">tambah</button>
+    <button @click="minusCount()">kurang</button>
 </template>
 <script>
 export default {
   data() {
     return {
-      message : "hello word"
+      message : "hello word",
+      count : 0
+    }
+  },
+  methods : {
+    minusCount () {
+      this.count -= 1
     }
   }
 }
