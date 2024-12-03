1d721ab7a4b78433b452092ed8ff491c0e8d0256
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-03 15:47:41 +0700

    input binding

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,25 @@
     <div>{{ count }}</div>
     <button @click="count++">tambah</button>
     <button @click="minusCount()">kurang</button>
+
+  <!-- input binding -->
+  <!-- 2 away binding -->
+    <div>{{ pesan }}</div>
+    <input 
+      :value="pesan"
+      @input="event => pesan = event.target.value">
+  <!-- v-model -->
+    <div>{{ pesan }}</div>
+    <hr>
+    <input type="text" v-model="pesan"><br>
 </template>
 <script>
 export default {
   data() {
     return {
       message : "hello word",
-      count : 0
+      count : 0,
+      pesan : "hello"
     }
   },
   methods : {
