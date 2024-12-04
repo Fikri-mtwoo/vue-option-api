dac07fe16a8cff2a3ac2ac085e03f44ba4eaf263
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-04 13:27:58 +0700

    passing props

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,23 @@
 <template>
   <h1>ini template parent</h1>
+  <HeaderTemplate title="hello word" />
+  <HeaderTemplate :title="message" />
+  <hr>
   <BasicTemplate />
 </template>
 
 <script>
   import BasicTemplate from './components/BasicTemplate.vue';
+  import HeaderTemplate from './components/HeaderTemplate.vue';
   export default {
     components : {
-      BasicTemplate
+      BasicTemplate,
+      HeaderTemplate
+    },
+    data() {
+      return {
+        message : "Title header"
+      }
     }
   }
 </script>
