27d6c772612e64319b825f89352ed8cf017273c4
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-04 19:09:25 +0700

    computed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <h1>ini template parent</h1>
-  <HeaderTemplate title="hello word" />
+  <!-- <HeaderTemplate title="hello word" /> -->
   <!-- <HeaderTemplate :title="message" /> -->
    <!-- <HeaderTemplate v-for="buah in buahs" :key="buah" :title="buah"/> -->
-  <hr>
+  <!-- <hr> -->
   <!-- <BasicTemplate /> -->
+   <BlankTemplate/>
 </template>
 
 <script>
   import BasicTemplate from './components/BasicTemplate.vue';
   import HeaderTemplate from './components/HeaderTemplate.vue';
+  import BlankTemplate from './components/BlankTemplate.vue';
   export default {
     components : {
       BasicTemplate,
-      HeaderTemplate
+      HeaderTemplate,
+      BlankTemplate
     },
     data() {
       return {
---
+++ src/components/BlankTemplate.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>{{ total }}</div>
+    <div>{{ banding }}</div>
+    <input type="number" v-model="numb1">
+    <input type="number" v-model="numb2">
+</template>
+<script>
+export default{
+    data() {
+        return {
+            numb1 : 0,
+            numb2 : 0
+        }
+    },
+    computed : {
+        total() {
+            return this.numb1 + this.numb2
+        },
+        banding() {
+            if(this.numb1 > this.numb2){
+                return "angka 1 lebih besar dari angka 2"
+            }else{
+                return "angka 2 lebih besar dari angka 1"
+            }
+        }
+    }
+}
+</script>
