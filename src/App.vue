038afc60742966ac03f767535bc2b165c0278951
fikri-mtwoo <ahmad.fikrinew2801@gmail.com>
2024-12-04 20:52:35 +0700

    emit dan refs

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,35 @@
    <!-- <HeaderTemplate v-for="buah in buahs" :key="buah" :title="buah"/> -->
   <!-- <hr> -->
   <!-- <BasicTemplate /> -->
-   <BlankTemplate/>
+   <!-- <BlankTemplate/> -->
+    <ChildTemplate ref="childComponent" @child="onPessParent"/>
+    <button @click="clickMe">parent button</button>
 </template>
 
 <script>
   import BasicTemplate from './components/BasicTemplate.vue';
   import HeaderTemplate from './components/HeaderTemplate.vue';
   import BlankTemplate from './components/BlankTemplate.vue';
+  import ChildTemplate from './components/ChildTemplate.vue';
   export default {
     components : {
       BasicTemplate,
       HeaderTemplate,
-      BlankTemplate
+      BlankTemplate,
+      ChildTemplate
     },
     data() {
       return {
         message : "Title header",
         buahs : ["jeruk", "apel", "melon", "semangka"]
+      }
+    },
+    methods : {
+      onPessParent(val){
+        console.log(`function dari parent ${val.nama}`)
+      },
+      clickMe(){
+        this.$refs.childComponent.onClick()
       }
     }
   }
---
+++ src/components/ChildTemplate.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>ini component child</div>
+    <button @click="onPress()">child button</button>
+</template>
+<script>
+export default{
+    data(){
+        return {
+            mhs : {
+                nama:"abdul",
+                jurusan:"Teknik Informatika"
+            }
+        }
+    },
+    emits : ['child'],
+    methods : {
+        onPress(){
+            this.$emit('child', this.mhs)
+        },
+        onClick(){
+            console.log('function dari child')
+        }
+    }
+}
+</script>
